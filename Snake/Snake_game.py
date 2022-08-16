347313bf83d4d024f8ed65e9f3f61a670dc6a04e
AliReza noroozy <a.noroozy95@gmail.com>
2022-08-16 17:41:14 +0430

    qe

--- Snake/Snake_game.py
+++ Snake/Snake_game.py
@@ -1,11 +1,11 @@
 import turtle
 import random
 
-
 Width = 500
 Height = 500
 Delay = 100
 Foodsize = 10
+
 offsets = {
     "up" : [0, 20],
     "down" : [0, -20],
@@ -23,37 +23,35 @@ def bind_direction_keys():
 def set_snake_direction(direction):
     global snake_direction
     if direction == 'up':
-        if direction != 'down':
-            snake_direction == 'Up'
-    elif direction == 'down':
-        if direction != 'up':
-            snake_direction == 'Down'
-    elif direction == 'right':
-        if direction != 'left':
-            snake_direction == 'Right'
-    elif direction == 'left':
-        if direction != 'right':
-            snake_direction == 'Left'
+        if snake_direction != 'down':
+            snake_direction = 'up'
+    if direction == 'down':
+        if snake_direction != 'up':
+            snake_direction = 'down'
+    if direction == 'right':
+        if snake_direction != 'left':
+            snake_direction = 'right'
+    if direction == 'left':
+        if snake_direction != 'right':
+            snake_direction = 'left'
 
 
 def game_loop():
     stamper.clearstamps()
 
     new_head = snake[-1].copy()
-    new_head[0] += offsets[snake_directions][0]
-    new_head[1] += offsets[snake_directions][1]
+    new_head[0] += offsets[snake_direction][0]
+    new_head[1] += offsets[snake_direction][1]
 
     if new_head in snake or (new_head[0] < - Width / 2) or (new_head[0] > Width / 2) \
         or (new_head[1] < - Height / 2 )or (new_head[1] > Height / 2):
         reset()
 
     else:
-
         snake.append(new_head)
 
         if not food_collision():
             snake.pop(0)
-
 
         for segment in snake:
             stamper.goto(segment[0], segment[1])
@@ -77,7 +75,6 @@ def food_collision():
     return False
 
 
-
 def get_random_food_pos():
     x = random.randint(- Width / 2 + Foodsize, Width / 2 - Foodsize)
     y = random.randint(- Height / 2 + Foodsize, Width / 2 - Foodsize)
@@ -87,40 +84,47 @@ def get_random_food_pos():
 def get_distance(pos1, pos2):
     x1, y1 = pos1
     x2, y2 = pos2
-    distance = (((x2 - x1) ** 2) + ((y2 - y1) ** 2)) ** 0.5
 
+    distance = (((x2 - x1) ** 2) + ((y2 - y1) ** 2)) ** 0.5
     return distance
 
 
-
+# reset game options
 def reset():
-    global snake, food_pos, snake_directions, score
+    global snake, food_pos, snake_direction, score
+    
     snake = [[0, 0], [20, 0], [40, 0], [60, 0]]
-    snake_directions = "up"
+    snake_direction = "up"
     score = 0
     food_pos = get_random_food_pos()
     food.goto(food_pos)
     game_loop()
 
+# screen setups
 screen = turtle.Screen()
 screen.setup(Width, Height)
-screen.title("Snake Game")
 screen.bgcolor("pink")
 screen.tracer(0)
+
+# event handler
 screen.listen()
 bind_direction_keys()
 
+# snake shape
 stamper = turtle.Turtle()
 stamper.shape("square")
 stamper.color("black")
 stamper.penup()
 
+# food shape
 food = turtle.Turtle()
 food.shape("circle")
 food.color("red")
 food.shapesize(Foodsize / 20)
 food.penup()
 
+# start game
 reset()
 
+# done game
 turtle.done()
